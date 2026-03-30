eb7d7cbcb76adffd2e83e6ab358559f006fd7dd1
sozkesenedwina <sozkesenedwina@gmail.com>
2026-03-30 10:37:33 +0200

    feat: Set correct base path for production builds
    Configures the Vite build to use a `/Apmusic/` base path when deploying to production environments, ensuring assets are correctly resolved. For development, it maintains the `./` base.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({mode}) => {
   return {
     root: 'src',
     publicDir: '../public',
-    base: './',
+    base: process.env.NODE_ENV === 'production' ? '/Apmusic/' : './',
     build: {
       outDir: '../',
       emptyOutDir: false,
